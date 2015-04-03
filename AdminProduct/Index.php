8eb805fe1b6fd6d2ba0ddf5b0acf26268c0f9ef4
TejinderGill <laddi_122005@hotmail.com>
2015-04-03 18:49:08 -0400

    Product

--- AdminProduct/Index.php
+++ AdminProduct/Index.php
@@ -1,11 +1,26 @@
 <?php
-    include '../Application/Class/IncludeClass.php';
-    LayoutClass::includeHeader();
-?>   
+    ob_start();
 
-        <?php
-            LayoutClass::includeHomeNav();
-        ?>
+include_once $_SERVER["DOCUMENT_ROOT"].'/project/itool/Application/Class/IncludeClass.php';
+
+include_once $_SERVER["DOCUMENT_ROOT"].'/project/itool/Application/Class/ValidationLibrary.php';
+
+include_once $_SERVER["DOCUMENT_ROOT"].'/project/itool/Application/Controller/AdminProductController.php';
+
+    LayoutClass::includeHeader();
+?>  
+
+<?php
+   LayoutClass::includeHomeNav();
+?>
+<?php
+  if(onsubmitCheck('submit')){
+      
+      
+      $controllerObj->formValues();
+      $controllerObj->insert();
+  }  
+?>
 
 <html>
     <head></head>
@@ -23,45 +38,26 @@
             </ul>
         </div>
         
+
         <div id="add_product">
             <h3>Add New Products</h3>
-            <form>
-                <label>Product Id:</label>  <input type="text" name="name" id="name" /><br/>
+            <form action="" method="post">
+                <label>Product Id:</label>  <input type="text" name="id" id="id" /><br/>
                 <label>Product Name:</label>  <input type="text" name="name" id="name" /><br/>
-                <label>Product Description:</label>  <textarea rows="3" cols="19" name="desc"></textarea><br/>
-                <label>Category Id:</label> 
-                <select name="P_category">  
-                    <option name="1" value="1">1</option>
-                    <option name="1" value="1">1</option>
-                    <option name="1" value="1">1</option>
-                </select><br/>
+                <label>Product Description:</label>  <textarea rows="3" cols="19" name="desc" id="desc" ></textarea><br/>
+                <label>Category Id:</label> <input type="text" name="catid" id="catid" /><br/>
                 <label>Buying Price</label> <input type="text" name="price" id="price" /><br/>
-                <label>Image:</label> <input type="file" name="image" id="image" /><br/>
+                <label>Image:</label> <input type="text" name="image" id="image" /><br/>
                 <input type="submit" name="submit" id="submit" value="Add Product" />
             </form>
         </div>
         <div id="view_product">
             <h3>View Products</h3>
             <form>
-                <table border="1">
-                    <tr>
-                        <th>Product Id</th>
-                        <th>Product Name</th>
-                        <th>Product Description</th>
-                        <th>Product Category</th>
-                        <th>Buying Price</th>
-                    </tr>
-                    <tr>
-                        <td>abc</td>
-                        <td>abc</td>
-                        <td>abc</td>
-                        <td>abc</td>
-                        <td>abc</td>
-                    </tr>
-                </table>
+                <?php $controllerObj->display(); ?>
             </form>           
         </div>
-        <div id="update_product">
+<!--        <div id="update_product">
             <h3>Update Product</h3>
             <form>
                 <label>Product Id:</label>  <input type="text" name="name" id="name" /><br/>
@@ -77,10 +73,24 @@
                 <label>Image:</label> <input type="file" name="image" id="image" /><br/>
                 <input type="submit" name="submit" id="submit" value="Update Product" />
             </form>
-        </div>
+        </div>-->
     </body>
 </html>
-
+        <?php        
+            if($_SERVER['QUERY_STRING'] !==""){
+            
+            echo '<div id = "result">';
+            
+            if($_SERVER['QUERY_STRING']==1)
+            { echo "Keyword has been added."; }
+            else
+                if($_SERVER['QUERY_STRING']==0)
+            { echo "Error in inserting keyword."; }
+            
+            }
+            
+            echo '</div>'
+        ?>
 
 <?php
     LayoutClass::includeFooter();
---
+++ Application/Model/ProductsModel.php
@@ -0,0 +1,143 @@
+<?php
+include $_SERVER["DOCUMENT_ROOT"].'/project/itool/Application/Class/DBAccessClass.php';
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+//class srtuc. with get set
+//class queries
+// all properties must be private
+
+class AddProduct{
+    
+    //class variables are same as db field names
+    private $product_id;
+    private $product_name;
+    private $product_description;
+    private $category_id;
+    private $buying_price;
+    private $image;
+    
+    function __construct($product_id, $product_name, $product_description, $category_id, $buying_price, $image)
+    {
+        $this->product_id=$product_id;
+        $this->product_name=$product_name;
+        $this->product_description=$product_description;
+        $this->category_id=$category_id;
+        $this->buying_price=$buying_price;
+        $this->image=$image;
+    }
+
+    public function getProduct_Id(){
+        return $this->product_id;        
+    }
+    
+    public function  setProduct_Id($value){
+        $this->product_id=$value;
+    }
+
+    public function getProduct_Name(){
+        return $this->product_name;
+    }
+    
+    public function  setProduct_Name($value){
+        $this->product_name=$value;
+    }
+
+    public function getProduct_Decription(){
+        return $this->product_description;        
+    }
+
+    
+    public function  setProduct_Description($value){
+        $this->product_description=$value;
+    }
+
+    public function getCategory_Id(){
+        return $this->category_id;
+    }
+    
+    public function setCategory_Id($value){
+        $this->category_id=$value;
+    }
+
+    public function getBuying_Price(){
+        return $this->buying_price;
+    }
+
+    public function setBuying_Price($value){
+        $this->buying_price=$value;
+    }
+
+    public function getImage(){
+        return $this->image;
+    }
+    
+    public function setImage($value){
+        $this->image=$value;
+    }  
+}
+
+class ProductFunctionalty{
+    
+     public $dbcon;
+    
+    public function __construct() {
+        
+        $obj = new AccessDB();
+        $this->dbcon = $obj->dbConnect();
+    }
+    
+    public function DisplayProduct(){
+        
+        $query="select product_id, product_name, product_description, category_id, "
+                . "buying_price, image from product_list";
+        
+        $statement = $this->dbcon->query($query);
+        
+        $statement->setFetchMode(PDO::FETCH_ASSOC);
+        
+         echo '<table border="1"> <th>Product Id</th><th>Product Name</th><th>Product Description'
+        . '</th><th>Category Id</th><th>Price</th><th>Image</th><th>Action</th>';
+         
+        foreach($statement as $q){
+           echo '<tr>';
+           echo '<td>'. $q['product_id'].'</td><td>'. $q['product_name'].'</td>'
+               . '<td>'.$q['product_description'].'</td>'
+               . '<td>'.$q['category_id'].'</td>'
+               . '<td>'.$q['buying_price'].'</td><td>'.$q['image'].'</td>'
+               . '<td><a href=update.php?id='.$q['category_id'].'>Update</a></td>';
+           echo '</tr>';  
+         }
+        echo '</table>';
+    }
+    
+    public function InsertProduct($newProd){
+        
+        $query=" insert into product_list (product_id, product_name, product_description,"
+                . " category_id, buying_price, image) values (:product_id, :product_name"
+                . ":product_description, :category_id, :buying_price, :image)";
+        
+        $statement = $this->dbcon->prepare($query);
+        
+        $statement->bindValue(':product_id', $newProd->getProduct_Id());
+        $statement->bindValue(':product_name', $newProd->getProduct_Name());
+        $statement->bindValue(':product_description', $newProd->getProduct_Decription());
+        $statement->bindValue(':category_id', $newProd->getCategory_Id());
+        $statement->bindValue(':buying_price', $newProd->getBuying_Price());
+        $statement->bindValue(':image', $newProd->getImage());
+                 
+        $success = $statement->execute();
+                 
+        $statement->closeCursor(); 
+        
+        if($success){
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
+}
