d072a63545f1ce7f7a1efaec929c4b7115dadbce
tejindergill91 <laddi_122005@hotmail.com>
2015-04-23 02:53:42 -0400

    CSS-category

--- Products/index.php
+++ Products/index.php
@@ -1,12 +1,28 @@
 <?php
-    include '../Application/Class/IncludeClass.php';
-    LayoutClass::includeHeader();
-?>
+ob_start();
 
- 
-        
-        <?php
-            LayoutClass::includeHomeNav();
+include_once $_SERVER["DOCUMENT_ROOT"].'/project/itool/Application/Class/IncludeClass.php';
+
+include_once $_SERVER["DOCUMENT_ROOT"].'/project/itool/Application/Class/ValidationLibrary.php';
+
+include_once $_SERVER["DOCUMENT_ROOT"].'/project/itool/Application/Controller/AdminProductController.php';
+
+    LayoutClass::includeHeader();
+
+    LayoutClass::includeHomeNav();
         ?>  
+<div id="tab">
+    <div id="heading">
+        <h1>Products</h1>
+    </div>
+    <hr/>
+    <div role="tabpanel" class="tab-pane" id="profile"> 
+<!--        <div id="view_product">-->
+        <h3>View Products</h3>
+            <form>
+                <?php $controllerObj->display(); ?>
+            </form>           
+    </div>
+</div>
 <?php
     LayoutClass::includeFooter();
