b239c7970571ec6e24c1f2ce5162e5d180ed7dca
ashwinpal <alagappan.pal@gmail.com>
2015-04-23 09:19:33 -0400

    bud fix

--- Application/Layout/navigation.php
+++ Application/Layout/navigation.php
@@ -6,7 +6,7 @@
                     <li><a href="<?php echo '../ImportList';?>"><i class="fa fa-shopping-cart fa" ></i></i> Purchase List</a></li> 
                     <li><a href="<?php echo '../TrackPurchase';?>"><i class="fa fa-shopping-cart fa" ></i></i> Track Purchase </a></li> 
                     <li><a href="<?php echo '../Invoices';?>"><i class="fa fa-money fa"></i> Invoices And Bills</a></li> 
-                    <li><a href="<?php echo '../Reports';?>"><i class="fa fa-list-alt fa"></i> Reports</a></li>
+                    <li><a href="<?php echo '../Report';?>"><i class="fa fa-list-alt fa"></i> Reports</a></li>
                     <li><a href="<?php echo '../Charts';?>"><i class="fa fa-bar-chart fa"></i> Charts</a></li>
                     <li><a href="<?php echo '../Rating';?>"><i class="fa fa-star fa"></i> Rate a Product</a></li>                     
                 </ul>
